45bc51d2d01e4d883146c5f8d985fee93c2e2ce5
Ishikawa Ryuto <ishikawa-r@protonmail.com>
2024-04-30 21:26:17 +0900

    Add tags to main VPC resource in main.tf

--- main.tf
+++ main.tf
@@ -8,6 +8,10 @@ resource "aws_vpc" "main" {
   cidr_block           = "10.0.0.0/16"
   enable_dns_support   = true
   enable_dns_hostnames = true
+
+  tags = {
+    Name = "main-vpc"
+  }
 }
 
 resource "aws_internet_gateway" "main" {
