a8ee7a3d57c10d8572a359e1db5a5628e8293bc1
Ishikawa Ryuto <ishikawa-r@protonmail.com>
2024-04-30 21:24:56 +0900

    Add depends_on attribute to aws_route resource in main.tf

--- main.tf
+++ main.tf
@@ -39,6 +39,7 @@ resource "aws_route" "public" {
   route_table_id         = aws_route_table.public.id
   gateway_id             = aws_internet_gateway.main.id
   destination_cidr_block = "0.0.0.0/0"
+  depends_on             = [aws_internet_gateway.main]
 }
 
 resource "aws_route_table_association" "public" {
