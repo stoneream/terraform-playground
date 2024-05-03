334942a4b9b7180224ebf1e1a71199ec9d27dbb5
Ishikawa Ryuto <ishikawa-r@protonmail.com>
2024-05-04 02:20:02 +0900

    Add IAM configuration for EC2 module

---
+++ modules/ec2/iam.tf
@@ -0,0 +1,29 @@
+resource "aws_iam_role" "ec2" {
+  name               = "ec2"
+  assume_role_policy = data.aws_iam_policy_document.assume_role.json
+}
+
+data "aws_iam_policy_document" "assume_role" {
+  statement {
+    actions = ["sts:AssumeRole"]
+
+    principals {
+      type        = "Service"
+      identifiers = ["ec2.amazonaws.com"]
+    }
+  }
+}
+
+data "aws_iam_policy" "policy_ssm_managed_instance_core" {
+  arn = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
+}
+
+resource "aws_iam_role_policy_attachment" "ssm_managed_instance_core" {
+  policy_arn = data.aws_iam_policy.policy_ssm_managed_instance_core.arn
+  role       = aws_iam_role.ec2.name
+}
+
+resource "aws_iam_instance_profile" "profile_private_instance" {
+  name = "profile_private_instance"
+  role = aws_iam_role.ec2.name
+}
--- modules/ec2/main.tf
+++ modules/ec2/main.tf
@@ -2,10 +2,11 @@
 
 resource "aws_instance" "private_instance" {
   # Amazon Linux 2 AMI (HVM) - Kernel 5.10, SSD Volume Type
-  ami             = "ami-04e0b6d6cfa432943"
-  instance_type   = "t2.micro"
-  subnet_id       = var.subnet_id
-  security_groups = [aws_security_group.ec2.id]
+  ami                  = "ami-04e0b6d6cfa432943"
+  instance_type        = "t2.micro"
+  subnet_id            = var.subnet_id
+  security_groups      = [aws_security_group.ec2.id]
+  iam_instance_profile = aws_iam_instance_profile.profile_private_instance.name
 
   tags = {
     Name = "private-instance"
