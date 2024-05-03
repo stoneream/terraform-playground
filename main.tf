a2967a95d92ec6bb90e3bae87b8bcebf1e25dd46
Ishikawa Ryuto <ishikawa-r@protonmail.com>
2024-05-04 00:31:31 +0900

    Add AWS provider configuration for EC2 module

--- main.tf
+++ main.tf
@@ -1,13 +1,3 @@
-provider "aws" {
-  region = "ap-northeast-1"
-}
-
-# resource "aws_instance" "example" {
-#   # Amazon Linux 2 AMI (HVM) - Kernel 5.10, SSD Volume Type
-#   ami           = "ami-04e0b6d6cfa432943"
-#   instance_type = "t2.micro"
-# }
-
 module "vpc" {
   source = "./modules/vpc"
 }
