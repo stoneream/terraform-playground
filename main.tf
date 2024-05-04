67476133741864967ab491d82bb8f7de03237f98
Ishikawa Ryuto <ishikawa-r@protonmail.com>
2024-05-04 17:33:34 +0900

    Enable GPU instance module and update IAM configuration

--- main.tf
+++ main.tf
@@ -11,13 +11,19 @@ module "s3_stable_diffusion" {
 module "ec2" {
   source = "./modules/ec2"
 
+  enabled   = false
   vpc_id    = module.vpc.vpc_id
   subnet_id = module.vpc.public_subnet_id
 }
 
-# module "gpu-instance" {
-#   source = "./modules/gpu-instance"
+module "gpu-instance" {
+  source = "./modules/gpu-instance"
 
-#   subnet_id = module.vpc.public_subnet_id
-#   vpc_id    = module.vpc.vpc_id
-# }
+  enabled   = true
+  subnet_id = module.vpc.public_subnet_id
+  vpc_id    = module.vpc.vpc_id
+
+  iam_policy_arns = [
+    module.s3_stable_diffusion.read_write_policy_arn
+  ]
+}
